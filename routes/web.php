163ff99087b33060e3f72db82f92fbe95888aa3d
destroylord <masapin68@gmail.com>
2024-05-26 09:46:31 +0700

    remove route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', )
+// Route::get('/', )
 
 Route::middleware('auth')->group(function () {
     Route::resource('obat', DrugController::class);
